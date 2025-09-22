5195b6edf9a64fe5a8d4c700ef28c1ca05cecc30
OEmiliatanO <emiliaistruelove@gmail.com>
2025-09-22 23:21:13 +0800

    add physical loss

--- lightning_model.py
+++ lightning_model.py
@@ -1,13 +1,31 @@
 import pytorch_lightning as pl
 import torch
 import torchmetrics
+from torch.nn.functional import cosine_similarity, mse_loss
 
-def physics_loss(pred, true, lam=0.1):
-    mse = torch.mean((pred - true)**2)
-    energy_pred = torch.sum(pred**2, dim=1)
-    energy_true = torch.sum(true**2, dim=1)
-    energy_mse = torch.mean((energy_pred - energy_true)**2)
-    return mse + lam * energy_mse
+def Lorenz_diff(X):
+    x, y, z = X[:,0], X[:,1], X[:,2]
+    dx = 10 * (y - x)
+    dy = x * (1 - z) - y
+    dz = x * y - (8/3) * z
+    dX = torch.stack([dx, dy, dz], dim=1)
+    return dX
+
+def energy_change_loss(pred, pre_gt, gt, dt):
+    # energy_pred = torch.sum(pred**2, dim=1)
+    # energy_true = torch.sum(true**2, dim=1)
+    # energy_mse = torch.mean((energy_pred - energy_true)**2)
+    # return energy_mse
+    pred_dX = (pred-pre_gt)/dt
+    true_dX = Lorenz_diff(pre_gt)
+    loss = torch.mean((2 * torch.sum(gt * pred_dX, dim=1) - 2 * torch.sum(gt * true_dX, dim=1))**2)
+    return loss
+
+def ODE_loss(pred, pre_gt, dt):
+    pred_dX = (pred-pre_gt)/dt
+    true_dX = Lorenz_diff(pre_gt)
+    loss = torch.mean((true_dX-pred_dX)**2)
+    return loss
 
 class LorenzLightningModule(pl.LightningModule):
     def __init__(self, model, lr=1e-3, lam=0.1, mode="naive"):
@@ -16,6 +34,7 @@ class LorenzLightningModule(pl.LightningModule):
         self.lr = lr
         self.lam = lam
         self.mode = mode
+        self.dt=0.01
         self.rmse = torchmetrics.MeanSquaredError(squared=False)
         self.acc = torchmetrics.CosineSimilarity()
         self.save_hyperparameters(ignore=["model"])
@@ -28,10 +47,12 @@ class LorenzLightningModule(pl.LightningModule):
         y_hat = self(x)
         if self.mode == "naive":
             loss = torch.mean((y_hat - y)**2)
-        else:  # physics guided
-            loss = physics_loss(y_hat, y, lam=self.lam)
-        rmse_val = self.rmse(y_hat, y)
-        acc_val = self.acc(y_hat, y)
+        elif self.mode == "ODE loss":
+            loss = torch.mean((y_hat - y)**2) + self.lam * ODE_loss(y_hat, x[:,-1], self.dt)
+        elif self.mode == "energy change loss":
+            loss = torch.mean((y_hat - y)**2) + self.lam * energy_change_loss(y_hat, x[:,-1], y, self.dt)
+        rmse_val = torch.mean(torch.sqrt(mse_loss(y_hat, y)))
+        acc_val = torch.mean(cosine_similarity(y_hat, y, dim=1))
         self.log(f"train_loss", loss)
         self.log(f"train_rmse", rmse_val)
         self.log(f"train_ACC", acc_val)
@@ -40,14 +61,18 @@ class LorenzLightningModule(pl.LightningModule):
     def validation_step(self, batch, batch_idx):
         x, y = batch
         y_hat = self(x)
-        rmse = torch.sqrt(torch.mean((y_hat - y)**2))
+        rmse = torch.mean(torch.sqrt(mse_loss(y_hat, y))) #torch.sqrt(torch.mean((y_hat - y)**2))
+        acc = torch.mean(cosine_similarity(y_hat, y, dim=1))
         self.log("val_rmse", rmse)
+        self.log("val_ACC", acc)
 
     def test_step(self, batch, batch_idx):
         x, y = batch
         y_hat = self(x)
-        rmse = torch.sqrt(torch.mean((y_hat - y)**2))
+        rmse = torch.mean(torch.sqrt(mse_loss(y_hat, y))) #rmse = torch.sqrt(torch.mean((y_hat - y)**2))
+        acc = torch.mean(cosine_similarity(y_hat, y, dim=1))
         self.log("test_rmse", rmse)
+        self.log("test_ACC", acc)
 
     def configure_optimizers(self):
         return torch.optim.Adam(self.parameters(), lr=self.lr)
--- train.py
+++ train.py
@@ -2,28 +2,56 @@ from pytorch_lightning import Trainer
 from lightning_model import LorenzLightningModule
 from lightning_data import LorenzDataModule
 from model import MLPModel, LSTMModel
+import numpy as np
+import logging
 
-# 假設你已經有 states = generate_lorenz_data(...)
+logging.getLogger("lightning.pytorch").setLevel(logging.ERROR)
 
-# train_split, val_split, test_split = 0.7, 0.15, 0.15
-# N = len(states)
-# train_data = states[:int(N*train_split)]
-# val_data   = states[int(N*train_split):int(N*(train_split+val_split))]
-# test_data  = states[int(N*(train_split+val_split)):]
+r_naive_model_metrics = []
+r_physics_model_metrics = []
 
-dm = LorenzDataModule(nc_file='data/lorenz_data_r=100.nc', normalize=True)
+R_values = [0.5, 1, 2, 5, 10, 28, 50, 100, 1000]
+#R_values = [100, 1000]
+for r in R_values:
+    naive_model_metrics = {'rmse':[], 'ACC':[]}
+    physics_model_metrics = {'rmse': [], 'ACC':[]}
+    dm = LorenzDataModule(nc_file=f'data/lorenz_data_r={r}.nc', normalize=True)
 
-# 選 MLP
-mlp = MLPModel(input_dim=30, hidden_dim=64, output_dim=3)
+    for _ in range(30):
+        mlp = MLPModel(input_dim=30, hidden_dim=64, output_dim=3)
 
-# Naive training
-naive_module = LorenzLightningModule(mlp, lr=1e-3, mode="naive")
-trainer = Trainer(max_epochs=20, accelerator="cpu")
-trainer.fit(naive_module, dm)
-trainer.test(naive_module, dm)
+        # Naive training
+        naive_module = LorenzLightningModule(mlp, lr=1e-3, mode="naive")
+        trainer = Trainer(max_epochs=20, accelerator="cpu", enable_progress_bar=False, enable_model_summary=False, logger=None)
+        trainer.fit(naive_module, dm)
+        trainer.test(naive_module, dm)
 
-# Physics-guided training
-phy_module = LorenzLightningModule(mlp, lr=1e-3, lam=0.1, mode="physics")
-trainer = Trainer(max_epochs=20, accelerator="cpu")
-trainer.fit(phy_module, dm)
-trainer.test(phy_module, dm)
+        naive_model_metrics['rmse'].append(trainer.callback_metrics['test_rmse'])
+        naive_model_metrics['ACC'].append(trainer.callback_metrics['test_ACC'])
+
+        # Physics-guided training
+        phy_module = LorenzLightningModule(mlp, lr=1e-3, lam=0.1, mode="energy change loss")
+        trainer = Trainer(max_epochs=20, accelerator="cpu", enable_progress_bar=False, enable_model_summary=False, logger=None)
+        trainer.fit(phy_module, dm)
+        trainer.test(phy_module, dm)
+
+        physics_model_metrics['rmse'].append(trainer.callback_metrics['test_rmse'])
+        physics_model_metrics['ACC'].append(trainer.callback_metrics['test_ACC'])
+    r_naive_model_metrics.append((naive_model_metrics['rmse'], naive_model_metrics['ACC']))
+    r_physics_model_metrics.append((physics_model_metrics['rmse'], physics_model_metrics['ACC']))
+
+print( "                       naive                        physics")
+print( "                   rmse            ACC             rmse            ACC")
+#print(f"r={r}    (X.XXXX±X.XXXX, X.XXXX±X.XXXX)  (X.XXXX±X.XXXX, X.XXXX±X.XXXX)")
+for i, r in enumerate(R_values):
+    naive_rmse_mean = np.mean(r_naive_model_metrics[i][0])
+    naive_rmse_std = np.std(r_naive_model_metrics[i][0])
+    naive_ACC_mean = np.mean(r_naive_model_metrics[i][1])
+    naive_ACC_std = np.std(r_naive_model_metrics[i][1])
+
+    physics_rmse_mean = np.mean(r_physics_model_metrics[i][0])
+    physics_rmse_std = np.std(r_physics_model_metrics[i][0])
+    physics_ACC_mean = np.mean(r_physics_model_metrics[i][1])
+    physics_ACC_std = np.std(r_physics_model_metrics[i][1])
+
+    print(f"r={r:.2f}    ({naive_rmse_mean:.4f}±{naive_rmse_std:.4f}, {naive_ACC_mean:.4f}±{naive_ACC_std:.4f})  ({physics_rmse_mean:.4f}±{physics_rmse_std:.4f}, {physics_ACC_mean:.4f}±{physics_ACC_std:.4f})")
