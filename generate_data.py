fix bug; optional normalizing data

generate_data.py
@@ -1,5 +1,6 @@
 import numpy as np
 import xarray as xr
+import os
 
 def lorenz_system(state, sigma=10.0, r=28.0, b=8/3):
     x, y, z = state
@@ -15,12 +16,12 @@ def rk4_step(f, state, dt, **kwargs):
     k4 = f(state + dt*k3, **kwargs)
     return state + (dt/6.0) * (k1 + 2*k2 + 2*k3 + k4)
 
-def generate_lorenz_data(T=2000, dt=0.01, sigma=10.0, rho=28.0, beta=8/3):
+def generate_lorenz_data(T=2000, dt=0.01, sigma=10.0, r=28.0, b=8/3):
     state = np.array([1.0, 1.0, 1.0])
     trajectory = np.zeros((T, 3))
     for t in range(T):
         trajectory[t] = state
-        state = rk4_step(lorenz_system, state, dt, sigma=sigma, rho=rho, beta=beta)
+        state = rk4_step(lorenz_system, state, dt, sigma=sigma, r=r, b=b)
 
     # 存成 xarray dataset
     ds = xr.Dataset(
@@ -30,9 +31,10 @@ def generate_lorenz_data(T=2000, dt=0.01, sigma=10.0, rho=28.0, beta=8/3):
     
     return ds
 
+os.makedirs("data", exist_ok = True)
 R_values = [5, 10, 20, 28, 40, 50, 100]
 for r in R_values:
     ds = generate_lorenz_data(T=2000, dt=0.01, r=r)
-    save_path = f"lorenz_data_r={r}.nc"
+    save_path = f"data/lorenz_data_r={r}.nc"
     ds.to_netcdf(save_path)
     print(f"Saved dataset to {save_path}")

lightning_data.py
@@ -2,36 +2,61 @@ import torch
 from torch.utils.data import Dataset, DataLoader
 import pytorch_lightning as pl
 import xarray as xr
+import numpy as np
 
 class LorenzDataset(Dataset):
-    def __init__(self, data, seq_len=10):
-        self.data = torch.tensor(data, dtype=torch.float32)
+    def __init__(self, data, seq_len=10, mean=None, std=None, normalize=False):
         self.seq_len = seq_len
+        self.normalize = normalize
+        self.data = torch.tensor(data, dtype=torch.float32)
+        if normalize and mean is not None and std is not None:
+            self.mean = torch.tensor(mean, dtype=torch.float32)
+            self.std = torch.tensor(std, dtype=torch.float32)
+            self.data = (self.data - self.mean) / (self.std + 1e-8)
+
+        self.X, self.Y = self.create_sequences(data, seq_len)
+
+    def create_sequences(self, states, seq_len):
+        X, Y = [], []
+        for i in range(len(states) - seq_len):
+            X.append(states[i:i+seq_len])
+            Y.append(states[i+seq_len])
+        return torch.tensor(np.array(X), dtype=torch.float32), torch.tensor(np.array(Y), dtype=torch.float32)
 
     def __len__(self):
-        return len(self.data) - self.seq_len
+        return len(self.X)
 
     def __getitem__(self, idx):
-        x = self.data[idx:idx+self.seq_len]
-        y = self.data[idx+1:idx+self.seq_len+1]
-        return x, y
+        return self.X[idx], self.Y[idx]
 
 class LorenzDataModule(pl.LightningDataModule):
-    def __init__(self, nc_file="lorenz.nc", seq_len=10, batch_size=32):
+    def __init__(self, nc_file="lorenz.nc", seq_len=10, batch_size=32, normalize=False):
         super().__init__()
         ds = xr.open_dataset(nc_file)
         self.data = ds["state"].values
         self.seq_len = seq_len
         self.batch_size = batch_size
+        self.normalize = normalize
 
     def setup(self, stage=None):
         N = len(self.data)
         train_size = int(N*0.8)
-        self.train_dataset = LorenzDataset(self.data[:train_size], self.seq_len)
-        self.val_dataset = LorenzDataset(self.data[train_size:], self.seq_len)
+        test_size = N - train_size
+
+        if self.normalize:
+            train_data = self.data[:train_size-64]
+            self.mean = train_data.mean(axis=0)
+            self.std = train_data.std(axis=0)
+
+        self.train_dataset = LorenzDataset(self.data[:train_size-64], self.seq_len, mean=self.mean, std=self.std, normalize=self.normalize)
+        self.val_dataset = LorenzDataset(self.data[train_size-64:train_size], self.seq_len, mean=self.mean, std=self.std, normalize=self.normalize)
+        self.test_dataset = LorenzDataset(self.data[train_size:], self.seq_len, mean=self.mean, std=self.std, normalize=self.normalize)
 
     def train_dataloader(self):
         return DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True)
 
     def val_dataloader(self):
         return DataLoader(self.val_dataset, batch_size=self.batch_size)
+    
+    def test_dataloader(self):
+        return DataLoader(self.test_dataset, batch_size=self.batch_size)

lightning_model.py
@@ -1,5 +1,6 @@
 import pytorch_lightning as pl
 import torch
+import torchmetrics
 
 def physics_loss(pred, true, lam=0.1):
     mse = torch.mean((pred - true)**2)
@@ -29,8 +30,8 @@ class LorenzLightningModule(pl.LightningModule):
             loss = torch.mean((y_hat - y)**2)
         else:  # physics guided
             loss = physics_loss(y_hat, y, lam=self.lam)
-        rmse_val = self.rmse(y_hat, y[:, -1])
-        acc_val = self.acc(y_hat, y[:, -1])
+        rmse_val = self.rmse(y_hat, y)
+        acc_val = self.acc(y_hat, y)
         self.log(f"train_loss", loss)
         self.log(f"train_rmse", rmse_val)
         self.log(f"train_ACC", acc_val)